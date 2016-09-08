36ccae28c84378b5906be369b41658f3d4e2f8c7
Dawson J Funk <df377@pvhs-lab112-d21.pvsd.org>
2016-09-08 14:29:36 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,49 @@
+package com.company;
+//Variable Notes
+public class Main {
+
+    public static void main(String[] args) {
+	//Three data types- to declare a variable
+        //String
+        //Int
+        //Double
+        String s;
+        s = "name"; //Initialize
+        //Must initialize FIRST then declare SECOND *exam question
+        String s2 = "nickname";
+        System.out.println(s);
+    //Naming rules of variables
+        // 1. if, else, void, public
+            //Reserved words
+            //Avoid all reserved words
+        // 2. Can't start with a number
+            //1dog is not good!
+        // 3. Special characters
+            //$#%
+        // 4. No spaces allowed
+            //First Name (not allowed)
+            //firstName first_name (ALLOWED)
+        // 5.  Can't reuse variable names
+
+    //String vs. Int
+        //Can't add string and int
+
+    //Int vs. Double
+        int a = 45;
+        double b = 3;
+        //Can't do int + double, unless sum is declared as double
+            //double sum = a + b
+        double sum = a + b;
+
+        a = 234567; //Don't declare variable as nt again, jut change value
+        int E,d = 0; //Calls two ints into existence at same time
+            //Uses commas
+        int sometime;
+        int thissssss = 0;
+
+        String something = ""; //Empty string
+        //String object pieces of it
+        //Strings have methods
+
+    }
+}
